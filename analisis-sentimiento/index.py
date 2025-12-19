54e0f0a4ccb552991f31ab7a2f7562caf5cd963d
justin <isaac_justin@hotmail.com>
2025-12-19 14:28:57 -0600

    last changes on lambda dev

--- analisis-sentimiento/index.py
+++ analisis-sentimiento/index.py
@@ -1,20 +1,28 @@
 import json
 import traceback
 from support.ComprehendClass import ComprehendClass
+from support.llm_class import LLMModel
 
 def handler(event, context):
     try:
         print(event)
         comprehend = ComprehendClass()
-        text = "Excelente lugar para entrenar, correr, caminar andar en bici o pasear al perro."
+        llm = LLMModel()
+        text = "PUS D PARTE D MI IYDER TENGO SIEMPRE 0RIENTASION NO SOLO PARA MI SINO TAMBIEN PARA TODO EL EKIPO Y SIEMPRE AY BUENA KIMICA PARA TODOS Y COMUNICASION"
         language = comprehend.detect_languages(text)
         language_code = language[0]["LanguageCode"]
         print(f"Language detected: {language}")
         sentiment = comprehend.detect_sentiment(text, language_code)
+        llm_sentiment = llm.invoke_llm(text)
+        print(f"LLM sentiment: {llm_sentiment}")
         print(f"Sentiment detected: {sentiment}")
         return {
             'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!')
+            "headers": {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*'
+            },
+            'body': json.dumps({"sentiment": sentiment})
         }
     except Exception as e:
         print(f"An error occurred at handler: {e}")
